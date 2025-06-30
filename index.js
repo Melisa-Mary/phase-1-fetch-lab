d610824628d28c3c0a621ecdc19cd13fac7fe3ab
Melisa-Mary <melisawanjiku93@gmail.com>
2025-06-30 13:02:14 +0300

    Finished fetch labs

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((books) => renderBooks(books));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+  const main = document.querySelector("main");
+
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// Make sure it runs on page load
+fetchBooks();
